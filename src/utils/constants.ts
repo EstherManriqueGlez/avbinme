9d95fb3c76ef5eee28866a018173bf708a1c7e6d
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-09-06 13:17:16 -0600

    fix: update phone number in CONTACT_INFO constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const CONTACT_INFO = {
   phones: [
     { number: "55 5588 9578", href: "tel:5555889578" },
-    { number: "55 2955 1257", href: "tel:5529551257" },
+    { number: "55 3965 4514", href: "tel:5539654514" },
   ],
   email: {
     address: "avaluosinmuebles10@gmail.com",
